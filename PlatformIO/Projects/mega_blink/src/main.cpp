cedb2d307375a9402e034477cd4824f870fe4412
phtrong <phtrongtue@gmail.com>
2021-03-01 23:23:20 +0100

    LOGIST-86

--- PlatformIO/Projects/mega_blink/src/main.cpp
+++ PlatformIO/Projects/mega_blink/src/main.cpp
@@ -4,7 +4,7 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
-// the loop function runs over and over again forever
+// the loop function runs over and over again forever#
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
   delay(500);                       // wait for half a second
