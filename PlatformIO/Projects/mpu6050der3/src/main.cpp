f55b75def5ea20ad53acd78a33628a4eece04752
phtrong <phtrongtue@gmail.com>
2021-10-11 22:21:21 +0200

    LOGIST-116

--- PlatformIO/Projects/mpu6050der3/src/main.cpp
+++ PlatformIO/Projects/mpu6050der3/src/main.cpp
@@ -1,121 +1,37 @@
 #include <Arduino.h>
-// I2C SRF10 or SRF08 Devantech Ultrasonic Ranger Finder
-// by Nicholas Zambetti <http://www.zambetti.com>
-// and James Tichenor <http://www.jamestichenor.net>
-
-// Demonstrates use of the Wire library reading data from the
-// Devantech Utrasonic Rangers SFR08 and SFR10
-
-// Created 29 April 2006
-
-// This example code is in the public domain.
-
-#include <Wire.h>
-
-void setup() {
-
-  Wire.begin();                // join i2c bus (address optional for master)
-
-  Serial.begin(9600);          // start serial communication at 9600bps
+// MPU-6050 Short Example Sketch
+// By Arduino User JohnChi
+// August 17, 2014
+// Public Domain
+#include<Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(9600);
 }
-
-int reading = 0;
-
-void loop() {
-
-  // step 1: instruct sensor to read echoes
-
-  Wire.beginTransmission(112); // transmit to device #112 (0x70)
-
-  // the address specified in the datasheet is 224 (0xE0)
-
-  // but i2c adressing uses the high 7 bits so it's 112
-
-  Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)
-
-  Wire.write(byte(0x50));      // command sensor to measure in "inches" (0x50)
-
-  // use 0x51 for centimeters
-
-  // use 0x52 for ping microseconds
-
-  Wire.endTransmission();      // stop transmitting
-
-  // step 2: wait for readings to happen
-
-  delay(70);                   // datasheet suggests at least 65 milliseconds
-
-  // step 3: instruct sensor to return a particular echo reading
-
-  Wire.beginTransmission(112); // transmit to device #112
-
-  Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
-
-  Wire.endTransmission();      // stop transmitting
-
-  // step 4: request reading from sensor
-
-  Wire.requestFrom(112, 2);    // request 2 bytes from slave device #112
-
-  // step 5: receive reading from sensor
-
-  if (2 <= Wire.available()) { // if two bytes were received
-
-    reading = Wire.read();  // receive high byte (overwrites previous reading)
-
-    reading = reading << 8;    // shift high byte to be high 8 bits
-
-    reading |= Wire.read(); // receive low byte as lower 8 bits
-
-    Serial.println(reading);   // print the reading
-
-  }
-
-  delay(250);                  // wait a bit since people have to read the output :)
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  delay(333);
 }
-
-/*
-
-// The following code changes the address of a Devantech Ultrasonic Range Finder (SRF10 or SRF08)
-
-// usage: changeAddress(0x70, 0xE6);
-
-void changeAddress(byte oldAddress, byte newAddress)
-
-{
-
-  Wire.beginTransmission(oldAddress);
-
-  Wire.write(byte(0x00));
-
-  Wire.write(byte(0xA0));
-
-  Wire.endTransmission();
-
-  Wire.beginTransmission(oldAddress);
-
-  Wire.write(byte(0x00));
-
-  Wire.write(byte(0xAA));
-
-  Wire.endTransmission();
-
-  Wire.beginTransmission(oldAddress);
-
-  Wire.write(byte(0x00));
-
-  Wire.write(byte(0xA5));
-
-  Wire.endTransmission();
-
-  Wire.beginTransmission(oldAddress);
-
-  Wire.write(byte(0x00));
-
-  Wire.write(newAddress);
-
-  Wire.endTransmission();
-
-}
-
-*/
